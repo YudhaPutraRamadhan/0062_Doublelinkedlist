2bed2f6bab7b11b97bac001513ca5e848d7f34a5
MzSuwawa <yudharamadhan838@gmail.com>
2024-05-04 14:18:39 +0700

    Menambahkan sebuah kondisi jika daftarnya kosong, jadikan new node sebagai START

--- Double Linked List/Double Linked List.cpp
+++ Double Linked List/Double Linked List.cpp
@@ -21,6 +21,10 @@ void addnode()
 
 	if (START == NULL || newNode->noMhs == START->noMhs)
 	{
-		
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDubplicate roll number not allowed\033[0m" << endl;
+			return;
+		}
 	}
 }
