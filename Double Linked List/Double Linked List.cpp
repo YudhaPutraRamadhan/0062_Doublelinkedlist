fbb0e5448d16d637ff313a54ca38e96575a55a9f
MzSuwawa <yudharamadhan838@gmail.com>
2024-05-04 14:37:40 +0700

    Menambahkan prosedur Boolean search (2)

--- Double Linked List/Double Linked List.cpp
+++ Double Linked List/Double Linked List.cpp
@@ -64,4 +64,13 @@ void addnode()
 	}
 }
 
-bool search(int rollNo, Node **previous, Node **current)
+bool search(int rollNo, Node** previous, Node** current)
+{
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+}
