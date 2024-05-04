029ae67734d8c7566c202488999f6d3112d4219a
MzSuwawa <yudharamadhan838@gmail.com>
2024-05-04 14:21:20 +0700

    Melakukan pembenaran commit yaitu jika daftarnya kosong, jadikan new node sebagai START

--- Double Linked List/Double Linked List.cpp
+++ Double Linked List/Double Linked List.cpp
@@ -26,5 +26,12 @@ void addnode()
 			cout << "\033[31mDubplicate roll number not allowed\033[0m" << endl;
 			return;
 		}
+		newNode->next = START;
+		if (START != NULL)
+		{
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
 	}
 }
